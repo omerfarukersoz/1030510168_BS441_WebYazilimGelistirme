f6f8c301779081c2cae45b89166cf6f5eabe157e
omerfarukersoz <1030510168@erciyes.edu.tr>
2022-11-25 23:00:18 +0300

    Dosyalar düzenlendi

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+const App = () => {
+
+    return (
+        <div>İlk Deneme</div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
